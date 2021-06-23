e5d745f10cc34d39ba34456691a358a982991caf
gmahima <mahimagangavarapu@gmail.com>
2021-06-23 15:46:37 +0530

    protocol inheritance

--- tutorial.playground/Contents.swift
+++ tutorial.playground/Contents.swift
@@ -1,22 +1,35 @@
 import UIKit
 
 //protocols
-protocol Identifiable {
-    var id: String {
-        get set
-        
+protocol Payable {
+    func calculateWages() -> Int
+}
+
+protocol NeedsTraining {
+    func study()
+}
+
+protocol HasVacation {
+    func takeVacation(days: Int)
+}
+
+protocol Employee: Payable, NeedsTraining, HasVacation {}
+
+struct StartupEmployee : Employee {
+    var salary: Int = 1000
+    func calculateWages() -> Int {
+        return 30*salary
+    }
+    func study() {
+        print("study study study")
+    }
+    func takeVacation(days: Int) {
+        print("Yay! I can relax for \(days) days.")
     }
 }
 
+var tatsumaki = StartupEmployee(salary: 10_000_000)
 
-struct Person: Identifiable {
-    var id: String
-}
-
-func displayID(thing: Identifiable) {
-    print(thing.id)
-}
-
-var a = Person(id: "1234")
-displayID(thing: a)
-
+tatsumaki.calculateWages()
+tatsumaki.study()
+tatsumaki.takeVacation(days: 500)
