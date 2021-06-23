07bea1982ba13d295951263c038775219cf9e497
gmahima <mahimagangavarapu@gmail.com>
2021-06-23 20:33:06 +0530

    add notes typecasting

--- tutorial.playground/Contents.swift
+++ tutorial.playground/Contents.swift
@@ -22,6 +22,7 @@ let animals = [Fish(), Dog(), Dog(), Fish()]
 for a in animals {
     if let dog = a as? Dog {
         dog.bark()
+        print(type(of: dog)) // converts type of animal to Dog (if possible)
     }
     else if let fish = a as? Fish {
         fish.swim()
