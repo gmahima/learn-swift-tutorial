d79167d7c3ea86a02749335dc8c0d4a8cc4b2aad
gmahima <mahimagangavarapu@gmail.com>
2021-06-23 20:19:12 +0530

    try? and try!

--- tutorial.playground/Contents.swift
+++ tutorial.playground/Contents.swift
@@ -1,28 +1,29 @@
 import UIKit
 
 
-//optional chaining
-
-var team7 = ["naruto", "sakura", "sasuke", "kakashi"]
-
-var first = team7.first?.uppercased()
-print(first) // Optional("NARUTO")
-
-var team700 = Array<String>()
-
-var first700 = team700.first?.uppercased()
-print(first700) // (nil)
-
-var inoShikaCho = ["Ino", "Shikamaru", "Chouji"]
-
-//var firIsc = inoShikaCho.first?.uppercased()
-//guard let firIsc = inoShikaCho.first?.uppercased() else {
-//
-//}
-//print(firstIsc) error can't find firstIsc in scope.
-
-var capIno:String = ""
-if let firIsc = inoShikaCho.first?.uppercased() {
-    capIno = firIsc
+//optional try
+enum PasswordError: Error {
+    case obvious
 }
-print(capIno)
+
+func checkPassword(_ password: String) throws -> Bool {
+    if password == "password" {
+        throw PasswordError.obvious
+    }
+
+    return true
+}
+
+//do {
+//    try checkPassword("password")
+//    print("That password is good!")
+//} catch {
+//    print("You can't use that password. \(error)")
+//}
+if let res = try? checkPassword("password") {
+    print("done. \(res)")
+} else {
+    print("you can't use that password")
+}
+let myBool = try! checkPassword("ultrasafepassword") // guess: try! is used when calling functions that can throw but you're sure that they are not going to throw.
+myBool
