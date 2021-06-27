bcc5952f60615375a1c3856835ae33c9df83ec36
gmahima <mahimagangavarapu@gmail.com>
2021-06-27 23:29:52 +0530

    first commit

---
+++ unit-conversion/unit-conversion/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  unit-conversion
+//
+//  Created by G H Mahimaanvita on 27/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        get {
+            return Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/) //Text is a struct that conforms to protocol: Equatable
+        }
+    }//body - a computed property
+}//ContentView - a struct
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ unit-conversion/unit-conversion/unit_conversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  unit_conversionApp.swift
+//  unit-conversion
+//
+//  Created by G H Mahimaanvita on 27/06/21.
+//
+
+import SwiftUI
+
+@main
+struct unit_conversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
